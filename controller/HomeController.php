5ef36bbe6db13fb8a37133ef75e31177d57cc0d9
MynameIsBoxxy <RoyScheider87@gmail.com>
2021-04-17 14:52:47 +0300

    Update HomeController.php

--- controller/HomeController.php
+++ controller/HomeController.php
@@ -12,10 +12,7 @@ class HomeController
         $this->container = $container;
     }
     public function home($request, $response, $args) {
-      // your code here
       
-      // use $this->view to render the HTML
-      //return $response;
       $model = new \App\Model\HomeModel($this->container);
       $data = $model->getAllObjects();
       $this->container->view->render($response, 'index.phtml', ['data'=>$data]);
